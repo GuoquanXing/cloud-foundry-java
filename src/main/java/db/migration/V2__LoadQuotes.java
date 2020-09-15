493e15092626fcc8b9ed697061923b9d34af0f04
Marco Spiess <marco.spiess01@sap.com>
2020-09-15 11:50:30 +0200

    Fix migration for jar

--- src/main/java/db/migration/V2__LoadQuotes.java
+++ src/main/java/db/migration/V2__LoadQuotes.java
@@ -1,21 +1,25 @@
 package db.migration;
 
-import java.net.URI;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.sql.PreparedStatement;
-
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 import org.springframework.util.ResourceUtils;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.sql.PreparedStatement;
+import java.util.stream.Collectors;
 
 public class V2__LoadQuotes extends BaseJavaMigration {
 
     @Override
     public void migrate(Context context) throws Exception {
-        URI uri = ResourceUtils.getFile("classpath:fortunes.txt").toURI();
-        String contents = new String(Files.readAllBytes(Paths.get(uri)));
-        String[] quotes = contents.split("(\r\n|\n|\r)~~~~~(\r\n|\n|\r)");
+        InputStream inputStream = ResourceUtils.getURL("classpath:fortunes.txt").openStream();
+        String contents = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))
+                .lines()
+                .collect(Collectors.joining("\n"));
+        String[] quotes = contents.split("\n~~~~~\n");
         String sql = "INSERT INTO quote (quote) VALUES (?)";
         try (PreparedStatement stmt = context.getConnection().prepareStatement(sql)) {
             for (String quote : quotes) {
